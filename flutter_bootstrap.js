e8fe91723615ef0188a6e8914b518a3582801e54
samuelkchris <samuelkchris@gmail.com>
2024-06-29 13:23:16 +0300

    updated sockets

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -21,7 +21,7 @@ const user_config = force_canvaskit ? {'renderer': 'canvaskit'} : {};
 _flutter.loader.load({
   config: user_config,
   serviceWorkerSettings: {
-    serviceWorkerVersion: "296137010",
+    serviceWorkerVersion: "64048014",
   },
   onEntrypointLoaded: async function(engine_initializer) {
     console.log('Flutter entrypoint loaded');
