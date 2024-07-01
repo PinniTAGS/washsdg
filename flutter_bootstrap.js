7f52aa27ff790e7600c917268ec5a787905f7d5b
samuelkchris <samuelkchris@gmail.com>
2024-07-01 12:08:41 +0300

    updatesmp

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -21,7 +21,7 @@ const user_config = force_canvaskit ? {'renderer': 'canvaskit'} : {};
 _flutter.loader.load({
   config: user_config,
   serviceWorkerSettings: {
-    serviceWorkerVersion: "64048014",
+    serviceWorkerVersion: "2102572569",
   },
   onEntrypointLoaded: async function(engine_initializer) {
     console.log('Flutter entrypoint loaded');
